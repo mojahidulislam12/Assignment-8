785c32879aaa8d11936bffb4842edb2c4eb12a87
Sagor <mojahidulislamsagor1234@gmail.com>
2026-04-29 21:53:31 +0600

    Nav and Not-found section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="text-2xl">Home</div>;
+  return <div className="text-5xl">Home</div>;
 }
---
+++ src/components/Share/NavBar.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import NavIcon from "@/assets/10433045.png";
+import user1 from "@/assets/user.png";
+import Image from "next/image";
+import Link from "next/link";
+const NavBar = () => {
+  const links = (
+    <>
+      <li className="font-normal text-[20px]">
+        <Link href={"/"}>Home</Link>
+      </li>
+      <li className="font-normal text-[20px]">
+        <Link href={"/all-books"}>All Books</Link>
+      </li>
+      <li className="font-normal text-[20px]">
+        <Link href={"/my-profile"}>My Profile</Link>
+      </li>
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar bg-base-100 max-w-285 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+
+          <Image
+            className="bg-white"
+            src={NavIcon}
+            width={45}
+            height={30}
+            alt=""
+          />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end flex gap-4">
+          <Image src={user1} width={41} height={41} alt="" />
+          <button className="btn">
+            <Link href={"/login"}>Login</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
