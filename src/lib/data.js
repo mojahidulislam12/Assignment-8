b201748c3661bd23674ce147a37e7cf035cf9aa3
Sagor <mojahidulislamsagor1234@gmail.com>
2026-05-03 16:05:41 +0600

    Change

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,6 @@
 export const getAllBooks = async () => {
   const res = await fetch("http://localhost:5000/books", {
-    cache: "force-cache",
+    cache: "no-store",
   });
   return res.json();
 };
