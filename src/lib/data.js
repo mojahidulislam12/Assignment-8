8cb68effe9dc30c96eae333e7f47c54db8bdab45
Sagor <mojahidulislamsagor1234@gmail.com>
2026-05-04 08:52:45 +0600

    json server

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,4 @@
 export const getAllBooks = async () => {
-  const res = await fetch("http://localhost:5000/books", {
-    cache: "no-store",
-  });
+  const res = await fetch(`https://assignment-8-server1111.onrender.com/books`);
   return res.json();
 };
